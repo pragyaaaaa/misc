faf829415a3f662a98ea0fabde3f82e6c93a01e3
pragyaaaaa <pragyasabstracts@gmail.com>
2020-12-01 01:38:36 +0530

    --forms

---
+++ forms/forms.js
@@ -0,0 +1,25 @@
+/*for (let button in buttons) {
+  console.log(button.value());
+} */
+function validateForm() {
+  let buttons = document.getElementsByTagName("button");
+  let inputs = document.querySelectorAll("input");
+  for (input in inputs) {
+    if (input.value !== "null") {
+      buttons[0].setAttribute("disabled", false);
+    } else {
+      console.log("Fields are not filled properly.");
+    }
+  }
+}
+
+function reactivate() {
+  let signInButton = document.querySelector("button");
+  if (signInButton.innerText === "Sign in") {
+    signInButton.setAttribute("innerText", "Sign Out");
+    //signInButton.innerText = "Sign Out";
+  } else {
+    signInButton.setAttribute("innerText", "Sign in");
+    console.log("It works.");
+  }
+}
