2461070a0a798778cd9192b962406012af11c329
pragyaaaaa <pragyasabstracts@gmail.com>
2020-12-04 15:46:21 +0530

    --progressbar

--- forms/forms.js
+++ forms/forms.js
@@ -2,6 +2,8 @@ let form = document.querySelector("#trial");
 let inputStateSelect = form.querySelector("#inputState");
 let inputStateSelectBtn = form.querySelector("#inputStateAdd");
 let stateChips = form.querySelector("#stateChips");
+let progress = form.querySelector("#progresstest");
+let progressbar = document.createElement("div");
 inputStateSelectBtn.addEventListener("click", (e) => {
   let temp = inputStateSelect.value;
   let chip = document.createElement("span");
@@ -25,3 +27,32 @@ inputStateSelectBtn.addEventListener("click", (e) => {
     inputStateSelect.options[idx].removeAttribute("disabled");
   });
 });
+
+let fillbtn = document.querySelector("#fillit");
+let inputEmail = document.querySelector("#inputEmail4");
+let inputpass = document.querySelector("#inputPassword4");
+
+fillbtn.addEventListener("click", (e) => {
+  form.style.opacity = 0.3;
+  setTimeout(() => {
+    progressTest();
+  }, 3000);
+  setTimeout(() => {
+    inputpass.value = "pragyasabstracts@gmail.com";
+  }, 4000);
+  setTimeout(() => {
+    progressEndTest();
+    form.style.opacity = 1;
+  }, 9000);
+  e.preventDefault();
+});
+
+function progressTest() {
+  progressbar.innerHTML =
+    '<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div> </div>';
+  progressbar.style.opacity = "unset";
+  progress.append(progressbar);
+}
+function progressEndTest() {
+  progressbar.innerHTML = " ";
+}
