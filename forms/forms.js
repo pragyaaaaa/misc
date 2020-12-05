2cd0cad2c3cd7e50927a8fccf5726f9b0ddd1452
pragyaaaaa <pragyasabstracts@gmail.com>
2020-12-05 20:47:42 +0530

    --conditional validations

--- forms/forms.js
+++ forms/forms.js
@@ -1,5 +1,6 @@
 let form = document.querySelector("#trial");
 let inputStateSelect = form.querySelector("#inputState");
+let notifSelect = form.querySelector("#notif");
 let inputZip = form.querySelector("#inputZip");
 let gridCheck = form.querySelector("#gridCheck");
 let inputCity = form.querySelector("#inputCity");
@@ -7,33 +8,51 @@ let formsub = form.querySelector("#formsub");
 let inputAddress2 = form.querySelector("#inputAddress2");
 let inputAddress = form.querySelector("#inputAddress");
 let inputStateSelectBtn = form.querySelector("#inputStateAdd");
+let notifAddBtn = form.querySelector("#notifAdd");
 let stateChips = form.querySelector("#stateChips");
+let notifChips = form.querySelector("#notifChips");
 let progress = form.querySelector("#progresstest");
 let progressbar = document.createElement("div");
-let success = document.querySelector("success");
+let success = document.querySelector("#success");
 let successbar = document.querySelector("div");
-inputStateSelectBtn.addEventListener("click", (e) => {
-  let temp = inputStateSelect.value;
+let zipcode = document.querySelector("#zipcode");
+
+function enableZip() {
+  if (stateChips.textContent.trim() === "Administrator") {
+    zipcode.classList.add("is-invalid");
+  }
+}
+function enableNotif() {
+  if (stateChips.textContent.trim() === "Administrator") {
+    notifSelect.classList.add("is-invalid");
+  }
+}
+
+function selectState(selectdrop, chiparea, callfn) {
+  let temp = selectdrop.value;
   let chip = document.createElement("span");
   chip.setAttribute("type", "button");
   chip.classList.add("badge", "badge-danger", "ml-4");
   chip.innerText = temp;
-  stateChips.append(chip);
-  let idx = inputStateSelect.selectedIndex;
+  chiparea.append(chip);
+  let idx = selectdrop.selectedIndex;
   if (temp === "1") {
-    inputStateSelect.setAttribute("disabled", true);
+    selectdrop.setAttribute("disabled", true);
   } else {
-    inputStateSelect.options[inputStateSelect.selectedIndex].setAttribute(
-      "disabled",
-      true
-    );
+    selectdrop.options[selectdrop.selectedIndex].setAttribute("disabled", true);
   }
-
   chip.addEventListener("click", () => {
     chip.remove();
-    inputStateSelect.removeAttribute("disabled");
-    inputStateSelect.options[idx].removeAttribute("disabled");
+    selectdrop.removeAttribute("disabled");
+    selectdrop.options[idx].removeAttribute("disabled");
   });
+  callfn();
+}
+inputStateSelectBtn.addEventListener("click", (e) => {
+  selectState(inputStateSelect, stateChips, enableNotif);
+});
+notifAddBtn.addEventListener("click", (e) => {
+  selectState(notifSelect, notifChips, enableZip);
 });
 
 let fillbtn = document.querySelector("#fillit");
@@ -47,6 +66,7 @@ fillbtn.addEventListener("click", (e) => {
   }, 3000);
   setTimeout(() => {
     inputpass.value = "pragyasabstracts@gmail.com";
+    inputEmail.value = "pragyasabstracts@gmail.com";
   }, 4000);
   setTimeout(() => {
     progressEndTest();
